0779e9da4d5ab488cc8568ddf85de4a9d13dc2f1
marisinka <marisinka_mail@mail.ru>
2016-03-25 00:15:30 +0200

    Added posibility of entry deletion

--- index.js
+++ index.js
@@ -4,25 +4,22 @@ var matter = require('gray-matter');
 var contentful = require('contentful');
 
 var options = {
-    accessToken: '0a8b3edce0196bb1f39dcc595084a58def2c45ce4c95c6cf953ef46d2a521d41',
-    space: 'ncdrvjovuu7s',
+    accessToken: 'bc13a61de5c57f651b0964e819c2c5c69325835c5f0d5a59360cf814db5af05b',
+    space: '1l8ci7f9k5d8',
     dataDirectory: './contentful-data',
     contentTypes: {
         Post: {
-            id: 'slug',
+            //id: 'slug',
             contents: 'body'
         },
-        Author: {
-
-        },
-        Category: {
-
-        }
+        Author: {},
+        Category: {}
     }
 };
 
 var entriesDirectory = options.dataDirectory + '/entries';
 var assetsDirectory = options.dataDirectory + '/assets';
+var syncDataFile = options.dataDirectory + '/sync-data.json';
 
 function mkdirIfNotExists(path) {
     var exists = false;
@@ -33,11 +30,11 @@ function mkdirIfNotExists(path) {
     } catch (err) {
     }
 
-    if(exists && !stats.isDirectory()) {
-        throw '"' + path + '" is not directory';
+    if (exists && !stats.isDirectory()) {
+        throw '"' + path + '" is not a directory';
     }
 
-    if(!exists) {
+    if (!exists) {
         fs.mkdirSync(path);
     }
 }
@@ -45,6 +42,18 @@ function mkdirIfNotExists(path) {
 mkdirIfNotExists(options.dataDirectory);
 mkdirIfNotExists(entriesDirectory);
 mkdirIfNotExists(assetsDirectory);
+
+var previousSyncData = null;
+try {
+    var syncDataStats = fs.statSync(syncDataFile);
+    if (syncDataStats.isFile()) {
+        var syncDataStr = fs.readFileSync(syncDataFile, 'utf8');
+        previousSyncData = JSON.parse(syncDataStr);
+    } else {
+        console.error('"' + syncDataFile + '" is not a file');
+    }
+} catch (err) {
+}
 
 var client = contentful.createClient({
     accessToken: options.accessToken,
@@ -55,44 +64,76 @@ var client = contentful.createClient({
 var spacePromise = client.getSpace();
 var contentTypesPromise = client.getContentTypes();
 var syncPromise = client.sync({
-    initial: true,
-    nextSyncToken: null,
-    resolveLinks: true
+    initial: !previousSyncData,
+    nextSyncToken: previousSyncData ? previousSyncData.nextSyncToken : null,
+    resolveLinks: false
 });
 
 Promise.all([spacePromise, contentTypesPromise, syncPromise])
-    .then(function(result) {
+    .then(function (result) {
         var space = result[0];
         var contentTypes = result[1];
         var syncData = result[2];
 
-        var defaultLocaleCode = space.locales.filter(function(locale) { return locale.default; })[0].code;
+        if(!previousSyncData) {
+            previousSyncData = {
+                nextSyncToken: null,
+                entries: {}
+            };
+        }
+        previousSyncData.nextSyncToken = syncData.nextSyncToken;
 
-        contentTypes.items.forEach(function(contentType) {
-            var contentTypeDirectory = entriesDirectory + '/' + changeCase.paramCase(contentType.name);
-            mkdirIfNotExists(contentTypeDirectory);
-        });
-
-        syncData.entries.forEach(function(entry) {
-            var contentTypeId = entry.sys.contentType.sys.id;
-            var contentType = contentTypes.items.filter(function (contentType) {
-                return contentType.sys.id === contentTypeId;
+        function getContentType(entry) {
+            var result = contentTypes.items.filter(function (contentType) {
+                return contentType.sys.id === entry.sys.contentType.sys.id;
             })[0];
 
-            var entryHash = {
+            if (!result) throw 'contentType id=' + entry.sys.contentType.sys.id + ' not found';
+            return result;
+        }
+
+        function getContentTypeDirectory(contentType) {
+            return entriesDirectory + '/' + changeCase.paramCase(contentType.name);
+        }
+
+        function getContentTypeEntryKey(contentType) {
+            if (options.contentTypes && options.contentTypes[contentType.name] && options.contentTypes[contentType.name].id) {
+                return options.contentTypes[contentType.name].id;
+            }
+            return 'id';
+        }
+
+        function getContentTypeContentsKey(contentType) {
+            if (options.contentTypes && options.contentTypes[contentType.name] && options.contentTypes[contentType.name].contents) {
+                return options.contentTypes[contentType.name].contents;
+            }
+            return null;
+        }
+
+        function getContentTypeEntryFileName(contentType, entryData) {
+            var contentTypeDirectory = getContentTypeDirectory(contentType);
+            var entryKey = getContentTypeEntryKey(contentType);
+            return contentTypeDirectory + '/' + entryData[entryKey] + '.md';
+        }
+
+        function getEntryData(contentType, entry) {
+            var data = {
                 id: entry.sys.id
             };
-            contentType.fields.forEach(function(field) {
+
+            contentType.fields.forEach(function (field) {
                 var value = null;
                 var fieldData = entry.fields[field.id];
 
-                if(fieldData) {
+                if (fieldData) {
                     fieldData = fieldData[defaultLocaleCode];
                     switch (field.type) {
                         case 'Array':
-                            switch (field.items.type){
+                            switch (field.items.type) {
                                 case 'Link':
-                                    value = fieldData.map(function (item) { return item.sys.id; });
+                                    value = fieldData.map(function (item) {
+                                        return item.sys.id;
+                                    });
                                     break;
                                 default:
                                     value = fieldData;
@@ -107,38 +148,55 @@ Promise.all([spacePromise, contentTypesPromise, syncPromise])
                             break;
                     }
 
-                    entryHash[field.id] = value;
+                    data[field.id] = value;
                 }
             });
 
+            return data;
+        }
+
+        var defaultLocaleCode = (space.locales || []).filter(function (locale) {
+            return locale.default;
+        })[0].code;
+
+        (contentTypes.items || []).forEach(function (contentType) {
             var contentTypeDirectory = entriesDirectory + '/' + changeCase.paramCase(contentType.name);
+            mkdirIfNotExists(contentTypeDirectory);
+        });
 
-            var entryKey = 'id';
-            var contentsKey = null;
-            if(options.contentTypes && options.contentTypes[contentType.name]) {
-                var contentTypeOptions = options.contentTypes[contentType.name];
+        (syncData.entries || []).forEach(function (entry) {
+            var contentType = getContentType(entry);
+            var data = getEntryData(contentType, entry);
 
-                if(contentTypeOptions.id) {
-                    entryKey = contentTypeOptions.id;
-                }
-
-                if(contentTypeOptions.contents) {
-                    contentsKey = contentTypeOptions.contents;
-                }
-            }
-
-            var yamlFileName = contentTypeDirectory + '/' + entryHash[entryKey] + '.md';
+            var contentsKey = getContentTypeContentsKey(contentType);
+            var entryFileName = getContentTypeEntryFileName(contentType, data);
 
             var yamlMatterContents = '';
-            if(contentsKey) {
-                yamlMatterContents = entryHash[contentsKey];
-                delete entryHash[contentsKey];
+            if (contentsKey) {
+                yamlMatterContents = data[contentsKey];
+                delete data[contentsKey];
             }
 
-            var yamlMatterStr = matter.stringify(yamlMatterContents, entryHash);
-            fs.writeFileSync(yamlFileName, yamlMatterStr);
+            var yamlMatterStr = matter.stringify(yamlMatterContents, data);
+            fs.writeFileSync(entryFileName, yamlMatterStr);
+
+            previousSyncData.entries[entry.sys.id] = entryFileName;
         });
+
+        (syncData.deletedEntries || []).forEach(function (deletedEntry) {
+            if (!previousSyncData) throw 'previousSyncData is null';
+
+            var entrylFileName = previousSyncData.entries[deletedEntry.sys.id];
+            fs.unlinkSync(entrylFileName);
+
+            delete previousSyncData.entries[deletedEntry.sys.id];
+        });
+
+        return previousSyncData;
     })
-    .catch(function(error) {
-       throw error;
+    .then(function(data) {
+        fs.writeFileSync(syncDataFile, JSON.stringify(data, null, 4));
+    })
+    .catch(function (error) {
+        throw error;
     });
